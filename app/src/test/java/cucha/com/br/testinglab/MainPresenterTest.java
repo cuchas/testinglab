57896a2ded21a3b9b85f66598c6590df4d8902fa
heitorcolangelo <heitor.colangelo@concrete.com.br>
2018-02-28 17:39:42 -0300

    Configura request matcher para retornar repostas mockadas

--- app/src/test/java/cucha/com/br/testinglab/MainPresenterTest.java
+++ app/src/test/java/cucha/com/br/testinglab/MainPresenterTest.java
@@ -1,8 +1,11 @@
 package cucha.com.br.testinglab;
 
 import android.support.annotation.NonNull;
-
+import br.com.concretesolutions.requestmatcher.LocalTestRequestMatcherRule;
+import br.com.concretesolutions.requestmatcher.RequestMatcherRule;
+import br.com.concretesolutions.requestmatcher.model.HttpMethod;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -15,9 +18,25 @@ import static junit.framework.TestCase.fail;
 @RunWith(JUnit4.class)
 public class MainPresenterTest {
 
+    @Rule
+    private final RequestMatcherRule server = new LocalTestRequestMatcherRule();
+    private FilmesAPI api;
+
     @Before
     public void setUp() {
+        String url = server.url("/").toString();
+        AppModule.setBaseUrl(url);
+        api = AppModule.getFilmesAPI();
         fail();
+    }
+
+    @Test
+    public void mockApiReturnExample() {
+        server.addFixture("response_200.json")
+                .ifRequestMatches()
+                .methodIs(HttpMethod.GET);
+
+        api.getFilmes().toBlocking();
     }
 
     @Test
